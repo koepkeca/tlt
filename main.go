bbc2b1bb0d5628c300f0d6cef1a66def36b97740
root <koepkeca@gmail.com>
2020-01-15 22:47:06 -0800

    Added random smoothing to simulate more realistic traffic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
@@ -12,6 +13,8 @@ import (
 	"github.com/koepkeca/tlt/config"
 	"github.com/koepkeca/tlt/metric"
 )
+
+const default_user_agent = "TLT - Trivial Load Tester github.com/koepkeca/tlt"
 
 func main() {
 	conf := config.New()
@@ -29,37 +32,43 @@ func main() {
 	}()
 	tt := time.Tick(conf.Interval)
 	st := time.Now()
-	totals := 1
+	rand.Seed(time.Now().UTC().UnixNano())
+	totalReq := int64(0)
 	for _ = range tt {
 		i := int64(0)
 		for i < conf.Requests {
-			log.Printf("Request %d\n",totals)
-			go func() {
+			log.Printf("Spawning request %d\n", totalReq)
+			go func(id int64) {
+				delay := time.Duration(rand.Int63n(conf.Interval.Milliseconds())) * time.Millisecond
+				time.Sleep(delay)
+				log.Printf("Sending request %d\n", id)
 				c := &http.Client{}
 				r, e := http.NewRequest("GET", conf.Target, nil)
 				if e != nil {
-					log.Printf("%s", e)
+					log.Printf("Error creating http request: %s", e)
 					return
 				}
+				r.Header.Set("User-Agent", default_user_agent)
 				resp, e := c.Do(r)
 				if e != nil {
-					log.Printf("%s", e)
+					log.Printf("Error performing http request:%s", e)
 					return
 				}
 				m.Process(resp)
 				resp.Body.Close()
-			}()
+			}(totalReq)
 			i++
-			totals++
+			totalReq++
 		}
 		et := time.Since(st)
 		if et > conf.Runtime {
 			break
 		}
 	}
-    log.Printf("Test concluded, terminating normally.")
+	log.Printf("Test concluded, terminating normally. Waiting for final requests to respond...")
 	time.Sleep(conf.Interval)
-	log.Printf("%s",m)
+	log.Printf("Done.\n")
+	log.Printf("%s", m)
 	m.Close()
 	return
 }
